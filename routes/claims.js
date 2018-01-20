e092409557bc7f243656279a511e3ee3e80fac4d
Andrew Rea <email@andrewrea.co.uk>
2018-01-20 16:00:42 +0000

    changed to node:alpine

--- routes/claims.js
+++ routes/claims.js
@@ -5,7 +5,7 @@ const router = express.Router();
 /* GET home page. */
 router.get('', (req, res) => {
   res.render('index', {
-    title: 'Submit a new claim',
+    title: 'Submit new claim',
   });
 });
 
